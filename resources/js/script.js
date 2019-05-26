d024e808ca8fc9bd4a39649f502a02a1e5bfe534
LeoTianJLL <leo.tian@ap.jll.com>
2019-05-26 19:34:55 +0800

    add responsive layout for navigation.

--- resources/js/script.js
+++ resources/js/script.js
@@ -91,4 +91,25 @@ $(document).ready(function() {
         offset: '50%'
     });
     
+    /* Mobile Navigation */
+    $('.js--nav-icon').click(function() {
+        var nav = $('.js--main-nav');
+        var icon = $('.js--nav-icon i');
+        
+        nav.slideToggle(200);
+        
+        if (icon.hasClass('ion-navicon-round')) {
+            icon.removeClass('ion-navicon-round');
+            icon.addClass('ion-close-round');
+        } else {
+            icon.addClass('ion-navicon-round');
+            icon.removeClass('ion-close-round');
+        }
+    })
+    
 })
+
+
+
+
+
