38049395d963ba14fc4b3f90667d70613bcfbf15
LeoTianJLL <leo.tian@ap.jll.com>
2019-05-26 18:53:20 +0800

    add animations.

--- resources/js/script.js
+++ resources/js/script.js
@@ -59,6 +59,36 @@ $(document).ready(function() {
         }
       });
     
-    /**/
+    /* Animations on Scroll */
+    
+    $('.js--wp-1').waypoint(function(direction) {
+        $('.js--wp-1').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2-1').waypoint(function(direction) {
+        $('.js--wp-2-1').addClass('animated fadeInLeft');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-2-2').waypoint(function(direction) {
+        $('.js--wp-2-2').addClass('animated fadeInRight');
+    }, {
+        offset: '50%'
+    });
+    
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+    
+     $('.js--wp-4').waypoint(function(direction) {
+        $('.js--wp-4').addClass('animated pulse');
+    }, {
+        offset: '50%'
+    });
     
 })
